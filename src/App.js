9c136829ced71e1fedaec9f8450c8c61eccb3d9b
Kamil Kaminski <kamil.kaminski3397@gmail.com>
2023-01-06 20:31:27 +0100

    making comment block and use ternary operators

--- src/App.js
+++ src/App.js
@@ -9,19 +9,30 @@ function App() {
     { id: 2, text: 'Comment two' },
     { id: 3, text: 'Comment three' },
   ]
+
+  const loading = false
+  const showComments = true
+
+  if (loading) return <h1>Loading...</h1>
+
+  const commentBlock = (
+    <div className="comments">
+      <h3>Comments({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  )
+
   return (
-    <dic className="container">
+    <div className="container">
       <h1>{title.toUpperCase()}</h1>
       <p>{body}</p>
-      <div className="comments">
-        <h3>Comments({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
-      </div>
-    </dic>
+
+      {showComments && commentBlock}
+    </div>
   )
 }
 
