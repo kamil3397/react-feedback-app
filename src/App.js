570430d4c221cf4819df2d3bca2bc3f1b05dfa29
Kamil Kaminski <kamil.kaminski3397@gmail.com>
2023-01-08 18:16:20 +0100

    header component and styles added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   return (
-    <dic className="container">
-      <h1>My App</h1>
-    </dic>
+    <>
+      <Header />
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
   )
 }
 
