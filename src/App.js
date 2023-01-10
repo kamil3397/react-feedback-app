352d44c56758c28869eaa704fe9f67b813e199bf
Kamil Kaminski <kamil.kaminski3397@gmail.com>
2023-01-10 05:50:37 +0100

    caching event from FeedbackItem

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import FeedbackList from './components/FeedbackList'
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
 
+  const deleteFeedback = (id) => {
+    console.log('App', id)
+  }
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   )
