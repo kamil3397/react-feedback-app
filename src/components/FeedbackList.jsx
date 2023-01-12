c740dd18ed737cf1592a522e5cf7fab5fe261b8f
Kamil Kaminski <kamil.kaminski3397@gmail.com>
2023-01-12 06:44:50 +0100

    framer-motion installed, optional animated version

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from 'framer-motion'
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
@@ -6,6 +7,22 @@ function FeedbackList({ feedback, handleDelete }) {
     return <p>No feedback yet</p>
   }
 
+  /////////////////////////////////////////////////////////////
+  //Animated version
+
+  // return (
+  //   <div className="feedback-list">
+  //     <AnimatePresence>
+  //       {feedback.map((item) => (
+  //         <motion.div key={item.id} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} layout>
+  //           <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //         </motion.div>
+  //       ))}
+  //     </AnimatePresence>
+  //   </div>
+  // )
+
+  ///////////////////////////////////////////////////////////
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
