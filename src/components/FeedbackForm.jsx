97539cffadb1ba281ac65ff9f571bbd419d483af
Kamil Kaminski <kamil.kaminski3397@gmail.com>
2023-01-12 06:27:26 +0100

    highlight rating fixed

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -41,7 +41,7 @@ function FeedbackForm({ handleAdd }) {
     <Card>
       <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
-        <RatingSelect select={(rating) => setRating(rating)} />
+        <RatingSelect select={setRating} selected={rating} />
         <div className="input-group">
           <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text} />
           <Button type="submit" isDisabled={btnDisabled}>
