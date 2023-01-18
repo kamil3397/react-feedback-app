fd7ada93d66fd09700243a3d8b7aa5883913a7df
Kamil Kaminski <kamil.kaminski3397@gmail.com>
2023-01-18 06:34:00 +0100

    comunication with backend done

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from 'uuid'
 import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
@@ -17,7 +16,7 @@ export const FeedbackProvider = ({ children }) => {
 
   //Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+    const response = await fetch(`/feedback?_sort=id&_order=desc`)
     const data = await response.json()
 
     setFeedback(data)
@@ -25,22 +24,42 @@ export const FeedbackProvider = ({ children }) => {
   }
 
   //Dodawanie feedback'u
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
+
+    setFeedback([data, ...feedback])
   }
 
   //Usuwanie feedback'u
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' })
+
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
   //Edytowanie itemu
 
-  const updateFeedback = (id, updItem) => {
-    setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item)))
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+    })
+    const data = await response.json()
+
+    setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...data } : item)))
   }
 
   //Ustawienie itemu do edycji
