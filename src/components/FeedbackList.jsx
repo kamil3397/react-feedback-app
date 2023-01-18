c8478919a435d8dcff6a97198e963952a3305f08
Kamil Kaminski <kamil.kaminski3397@gmail.com>
2023-01-18 07:11:03 +0100

    comment for import

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,4 @@
-import { motion, AnimatePresence } from 'framer-motion'
+// import { motion, AnimatePresence } from 'framer-motion'
 import { useContext } from 'react'
 import FeedbackItem from './FeedbackItem'
 import Spinner from './shared/Spinner'
