ad50c1a965261032aa052db46868d2ab2e056464
Kamil Kaminski <kamil.kaminski3397@gmail.com>
2023-01-18 06:14:04 +0100

    making db and loading animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,12 +1,13 @@
 import { motion, AnimatePresence } from 'framer-motion'
 import { useContext } from 'react'
 import FeedbackItem from './FeedbackItem'
+import Spinner from './shared/Spinner'
 import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackList({}) {
-  const { feedback } = useContext(FeedbackContext)
+  const { feedback, isLoading } = useContext(FeedbackContext)
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No feedback yet</p>
   }
 
@@ -26,7 +27,9 @@ function FeedbackList({}) {
   // )
 
   ///////////////////////////////////////////////////////////
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       {feedback.map((item) => (
         <FeedbackItem key={item.id} item={item} />
